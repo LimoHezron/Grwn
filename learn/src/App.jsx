8a6425e39fa08c90b0377683cba8f46fbb64dbae
LimoHezron <kibungeihezron@gmail.com>
2026-01-13 23:14:19 +0300

    more than one prop

--- learn/src/App.jsx
+++ learn/src/App.jsx
@@ -6,10 +6,10 @@ function App() {
   return (
     <> 
       <TodoTitle />
-      <Todo task="Learn React"/>
-      <Todo task="Finish ASAP Frontend"/>
-      <Todo  task="Land a Junior Job" />
-      <Todo  task="Earn $100k+ " />
+      <Todo task="Learn React" description="Code along and take notes"/>
+      <Todo task="Finish ASAP Frontend" description="3hrs everyday" />
+      <Todo  task="Land a Junior Job" description="Apply job 2hrs everyday" />
+      <Todo  task="Earn $100k+ " description="Live a life and relax!" />
     </>
   );
 }
--- learn/src/components/Todo.jsx
+++ learn/src/components/Todo.jsx
@@ -1,9 +1,11 @@
-function Todo({task}){
+function Todo({task, description}){
     return(
         <>
         <div className="todo-item">
             <h2> {task} </h2>
+            <p>{description}</p>
             <button>Delete</button>
+
         </div>
         </>
     )
